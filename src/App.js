a65d822cb9e4ce16f428ab80e308b61cd94b6781
marianasantos3 <85956510+marianasantos3@users.noreply.github.com>
2022-05-01 12:21:19 -0300

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,41 @@
+import './App.css';
+
+
+function App() {
+  return (
+
+    <main className='Main'>
+
+    <div className="Cabecalho">
+       <h1> DESENHOS ANIMADOS </h1>
+       
+      
+      <div className="Menu">
+					<button type="button" class="btn btn-outline-dark">Entre ou cadastre-se</button>
+		</div>
+         </div>
+
+          <div className="Galeria"></div>
+
+          <div className="Galeria1"> <p> The Simpsons é amplamente considerada como uma das maiores séries de televisão de todos os tempos. A edição da revista Time publicada em 31 de dezembro de 1999 classificou a série como a melhor do século XX,[11] e em 14 de janeiro de 2000, a animação foi homenageada com uma estrela na Calçada da Fama de Hollywood, na Califórnia. A série já venceu inúmeros prêmios desde a sua estreia, incluindo 31 Primetime Emmy Awards, 30 prêmios Annie e um prêmio Peabody. A expressão utilizada por Homer, "D'oh", foi incluída no Oxford English Dictionary, enquanto a série tem influenciado várias sitcoms direcionadas ao público adulto. </p></div>
+
+            
+            <div className="Conteudo"> <h2> O que você vai ver primeiro?</h2> </div>
+            <div className="Modo"> </div>
+            <br></br>
+          
+            <div className="Modo2">  </div>
+
+              <div className='Modo3'> </div>
+            
+            <div className='Modo4'>  <p>Os desenhos animados são o fascínio das crianças e até mesmo dos adultos. O desenho animado ou a animação é o processo pelo qual os desenhos ‘manuais’ ganham vida, ou seja, ganham movimentos e assim pode formar histórias animadas. Grandes desenhistas como Walt Disney utilizaram este processo para dar ‘vida’ aos seus desenhos até então nascidos apenas numa simples folha de papel. A animação é feita por fotogramas. Cada um deles é feito separadamente e assim podem ser fotografados através de um programa de computação gráfica que cria os movimentos e dá vida ao desenho, fazendo-o andar, sorrir, falar e etc. </p></div>
+
+            <div className="Rodape"> <h3> ANUNCIOS PERSONALIZADOS </h3>
+            <h1> Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in </h1> </div>
+
+    </main>
+
+  );
+}
+
+export default App;
